75244d9080fce88762f08751bf38ab432459f668
FunKodeT <cooteybug@gmail.com>
2024-04-22 16:56:50 -0600

    1.0; Added: 1(BRANCH), controllers\, routes\, index.js(controllers\), index.js(routes\); Changed: index.js(Server\)=server.js(Server\); Updated: index.js(controllers\), index.js(routes\), server.js(Server\), package.json(Server\), README.txt(1700M1NodeJS); Removed: Test(1700M1NodesJS\)&+;

--- Server/server.js
+++ Server/server.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
-app.get("/",(req,res)=>{
-    res.send("Hello World!");
-});
-app.listen(3000,()=>{
+app.use("/",require("./routes/index"));
+app.listen(PORT,()=>{
     console.log(`Test server running on port: ${PORT}`);
 });
